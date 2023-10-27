505f04c3cd0ef537c362b0c333e679c488384c48
JASmedley <jasmine.a.smedley@gmail.com>
2023-10-26 18:08:00 -0700

    adding the three diffferent parameter set ups

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let express = require("express")
 let PORT = 8001
 let app = express();
+app.use(express.json())
 
 //VERBS: Get, Post, Delete, Put 
 
@@ -11,9 +12,6 @@ let app = express();
 
 //there are 3 different ways to pass in information from front end to the back end 
 // 1. query parameter (if information is not required)
-// 2. Route or Path parameter (if information is required)
-
-
 app.get("/hello", function (req,res){
   let name = req.query.name;
   let title = req.query.title;
@@ -34,10 +32,34 @@ app.get("/hello", function (req,res){
 
 });
 
+
+// 2. Route or Path parameter (if information is required)
 app.post("/hello/:name",function(req, res){
   let name = req.params.name
   res.send(`Hello ${name} how are you today? This is a route or path parameter.`);
 });
+
+
+// 3. Request body (if information is not required)
+app.put("/hello", function(req,res){
+  let name = req.body.name;
+  let title = req.body.title;
+
+  let fullName;
+  if(name && title){
+    fullName = title+" "+name;
+  }
+  else if (name){
+    fullName = name;
+  }
+  else if (title){
+    fullName = title;
+  } else {
+    fullName = "Buddy"
+  }
+  res.send(`Hello There ${fullName}. It's a nice day, yes?`);
+})
+
 
 app.listen(PORT, function (){
   console.log("Application Started on Port", PORT)
